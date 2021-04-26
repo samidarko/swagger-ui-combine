f0ea40dc72852ef5400575b4923e2282e45f09c0
samidarko <samidarko@gmail.com>
2021-04-26 11:59:32 +0200

    replace swagger by openapi version

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const INFO_VERSION = process.env.INFO_VERSION || "0.1.0"
 const APIS = process.env.APIS || "[]"
 
 const config = {
-    "swagger": "2.0",
+    "openapi":"3.0.2",
     "info": {
         "title": INFO_TITLE,
         "version": INFO_VERSION
